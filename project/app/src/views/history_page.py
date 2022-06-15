85a981a3b79a94182a509724fd2309a6200cbbf3
Felix Gomez <61999750+FelixDavid12@users.noreply.github.com>
2022-06-14 21:57:30 -0400

    feat: create the real-time app prototype (#17)
    * feat(packages): modify requirements and add binaries for Windows
    
    * refactor: add node_modules to .gitignore
    
    * feat: create first prototype of the real-time application
    
    Install Tailwind and implement with tailwind.config.js.
    
    Configure files structure of the app.
    
    Create languages folders for internationalization.
    
    Co-authored-by: Felixdavid12 <felixdavidgom@gmail.com>

---
+++ project/app/src/views/history_page.py
@@ -0,0 +1,34 @@
+import json
+
+import pandas as pd
+from dash import html, dcc
+import plotly.express as px
+
+f_content = open('languages/en/history.json')
+content = json.load(f_content)
+f_content.close()
+
+
+def get_page():
+    df = pd.DataFrame({'lat': [4.651981275958889], 'lon': [-74.07677205983546]})
+
+    fig = px.scatter_mapbox(df, lat="lat", lon="lon", zoom=10)
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+    return html.Div([
+        html.H1(content['title'], className='text-2xl font-bold'),
+        html.Div([
+            html.H2(content['card_1']['title'], className='text-lg'),
+            html.P(content['card_1']['text']),
+        ], className='card'),
+        html.Div([
+            html.Div([
+                html.H2(content['card_2']['title'], className='text-lg'),
+                html.Div(
+                    dcc.Graph(figure=fig, className='h-[40rem] w-full border-gray-300 border-2 rounded-md'),
+                    className=''
+                ),
+            ], className='card w-full flex flex-col gap-4'),
+        ], className='h-full'),
+    ], className='mx-auto container space-y-6 h-full')
---
+++ project/app/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.py"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
