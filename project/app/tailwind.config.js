e90e6e79e68d9c6a2b0f3c8997d17b264cb99694
FelixDavid12 <felix.david@yahoo.com>
2022-06-22 15:03:12 -0400

    feat: install postcss and create scripts to use it
    In production use npm run build.
    
    In development use npm run hot.

--- project/app/tailwind.config.js
+++ project/app/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.py"],
+  content: [
+    "./src/views/*.py",
+    "./src/**/*.py"
+  ],
   theme: {
     extend: {},
   },
