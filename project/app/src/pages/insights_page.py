feb243d8d79ca34f772545eec067314ef5e695e0
Felix Gomez <61999750+FelixDavid12@users.noreply.github.com>
2022-07-05 22:03:00 -0400

    feat: implement sliders for the insights that require it (#36)
    Co-authored-by: Felixdavid12 <felixdavidgom@gmail.com>

--- project/app/src/pages/insights_page.py
+++ project/app/src/pages/insights_page.py
@@ -1,6 +1,6 @@
 import json
 
-from dash import html, dcc, register_page
+from dash import html, dcc, register_page, Output, Input, callback
 from src_code.insights import Insights
 
 
@@ -10,21 +10,23 @@ f_content = open('languages/en/insights.json')
 content = json.load(f_content)
 f_content.close()
 
+insights: Insights | None = None
+
+colors = {
+    'background': '#1E293B',
+}
+
 
 def layout():
+    global insights
+
     insights = Insights()
 
     insights_to_display = [
         insights.biggest_accidents_per_type(),
         insights.accidents_per_vehicle_type(),
-        insights.accidents_per_location(),
-        insights.accidents_per_zone_and_hour(hour=0),
-        insights.accidents_per_hour(hour=0)
+        insights.accidents_per_location()
     ]
-
-    colors = {
-        'background': '#1E293B',
-    }
 
     for _, fig, _ in insights_to_display:
         fig.update_layout(
@@ -33,6 +35,17 @@ def layout():
             font_color='#ffffff'
         )
 
+    html_insights = [
+        html.Div([
+            html.Div(
+                dcc.Graph(figure=fig, className='h-full w-full border-gray-300 border-2 rounded-md'),
+                className=''
+            ),
+            html.P(" & ".join(insight))
+        ], className='card w-full flex flex-col gap-4')
+        for data, fig, insight in insights_to_display
+    ]
+
     return html.Div([
         html.H1(content['title'], className='text-2xl font-bold'),
         html.Div([
@@ -40,13 +53,80 @@ def layout():
             html.P(content['card_1']['text']),
         ], className='card'),
         html.Div([
+            *html_insights,
             html.Div([
-                html.Div(
-                    dcc.Graph(figure=fig, className='h-full w-full border-gray-300 border-2 rounded-md'),
-                    className=''
-                ),
-                html.P(" & ".join(insight))
-            ], className='card w-full flex flex-col gap-4')
-            for data, fig, insight in insights_to_display
+                html.Div([], className='w-full h-full', id='graph_slider_1'),
+                html.P(content.get('slider')),
+                html.Div([
+                    dcc.Slider(
+                        0, 23, 1,
+                        value=10,
+                        id='slider_graph_1'
+                    ),
+                ]),
+                html.P('', id='insight_graph_1')
+            ], className='card w-full flex flex-col gap-4'),
+            html.Div([
+                html.Div([], className='w-full h-full', id='graph_slider_2'),
+                html.P(content.get('slider')),
+                html.Div([
+                    dcc.Slider(
+                        0, 23, 1,
+                        value=10,
+                        id='slider_graph_2'
+                    ),
+                ]),
+                html.P('', id='insight_graph_2')
+            ], className='card w-full flex flex-col gap-4'),
         ], className='h-full grid grid-cols-1 lg:grid-cols-2 gap-4 h-full'),
     ], className='mx-auto container space-y-6 h-full')
+
+
+@callback(
+    output=[Output('graph_slider_1', 'children'), Output('insight_graph_1', 'children')],
+    inputs=[Input('slider_graph_1', 'value')]
+)
+def update_graph_1(value):
+    fig_to_return = None
+    insight = ''
+
+    if insights is not None:
+        _, fig, insight = insights.accidents_per_zone_and_hour(hour=int(value))
+
+        fig.update_layout(
+            plot_bgcolor=colors['background'],
+            paper_bgcolor=colors['background'],
+            font_color='#ffffff'
+        )
+
+        fig_to_return = html.Div(
+            dcc.Graph(figure=fig, className='h-full w-full border-gray-300 border-2 rounded-md'),
+            className=''
+        )
+
+    return fig_to_return, " & ".join(insight)
+
+
+@callback(
+    output=[Output('graph_slider_2', 'children'), Output('insight_graph_2', 'children')],
+    inputs=[Input('slider_graph_2', 'value')]
+)
+def update_graph_2(value):
+    fig_to_return = None
+    insight = ''
+
+    if insights is not None:
+        _, fig, insight = insights.accidents_per_hour(hour=int(value))
+
+        fig.update_layout(
+            plot_bgcolor=colors['background'],
+            paper_bgcolor=colors['background'],
+            font_color='#ffffff'
+        )
+
+        fig_to_return = html.Div(
+            dcc.Graph(figure=fig, className='h-full w-full border-gray-300 border-2 rounded-md'),
+            className=''
+        )
+
+    return fig_to_return, " & ".join(insight)
